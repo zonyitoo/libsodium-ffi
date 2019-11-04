316988f0f70851f22fd3a4bbef6b3de2abac0fce
zonyitoo <zonyitoo@gmail.com>
2019-11-05 01:09:53 +0800

    Suppress long double to u128 FFI safe warning

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
+#![allow(improper_ctypes)] //  warn long double -> u128 is not FFI-safe
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
